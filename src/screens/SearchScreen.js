a04f941400ef43e25d8e0bca05c06e3234cefcb8
Zeztron <pateltharsh@gmail.com>
2019-10-08 21:22:45 -0400

    rendered flatlist of restaurant names

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,21 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
+
+  const filterResultsByPrice = price => {
+    return results.filter(result => {
+      return result.price === price
+    });
+  };
   
   return (
     <View>
-      <SearchBar 
-        term={term} 
+      <SearchBar
+        term={term}
         onTermChange={setTerm}
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice("$")} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice("$$")} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice("$$$")} title="Big Spender" />
     </View>
   );
 };
